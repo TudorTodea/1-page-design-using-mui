84d29c5c099633f5be149a2067315f93a925fe68
TudorTodea <tudortodea1416@gmail.com>
2022-12-05 05:31:08 +0200

    added navbar,leftbar,rightbar,and feed+ responsivenes using mui breakpoints

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Box, Container, Stack } from "@mui/material";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Box>
+      <Navbar />
+      <Stack direction="row" spacing={2} justifyContent='space-between'>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
